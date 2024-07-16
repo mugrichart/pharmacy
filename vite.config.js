468386c447b30767c7bac99d66fb320fecff8f9b
Richard <mugrich007@gmail.com>
2024-07-16 20:43:33 +0000

    outDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+        outDir: 'dist' // Ensure this is set to 'dist' or your desired output directory
+    },
     server: {
         port: 3000
     }
